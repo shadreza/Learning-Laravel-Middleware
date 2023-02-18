444ecc91f4d1df24b421186796469e1841245f17
shadreza <shadreza100@gmail.com>
2023-02-18 23:37:46 +0600

    added array of middleares

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('admin/user/roles', ['middleware' => ['role', 'auth'], function () {
+Route::get('admin/user/roles', ['middleware' => ['role', 'auth', 'web'], function () {
     return "Role Middleware";
 }]);
