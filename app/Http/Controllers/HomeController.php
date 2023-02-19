4297344d2107e4d02ce324fa1961c16d5fccd5c5
shadreza <shadreza100@gmail.com>
2023-02-19 11:42:36 +0600

    sessions added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,17 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+
+
+    // adding sessions
+    public function index(Request $request)
     {
-        return view('home');
+        // session added
+        // $request->session()->put(['shad' => 'master instructor']);
+        // session(['tuba' => 'wife']);
+
+        return $request->session()->all();
+
+        // return view('home');
     }
 }
